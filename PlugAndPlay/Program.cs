ad9021c61b1040fa1c6a240db3339886b3d7ee10
Arjikaytz <armelmacayan13@gmail.com>
2025-03-22 01:07:59 +0800

    Add project files.

---
+++ PlugAndPlay/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PlugAndPlay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
